f685c0b3653aad0352d19f6934e04e14cc3e30ba
Greenfz <45266820+Greenfz@users.noreply.github.com>
2020-02-12 09:41:07 +0100

    maven package

--- src/main/java/ProcesorXML.java
+++ src/main/java/ProcesorXML.java
@@ -19,19 +19,19 @@ public class ProcesorXML {
     public static void main(String[] args) {
         String inputFilePath = "src/main/resources/input.xml";
         String outputFilePath = "src/main/resources/output.xml";
+
         FromXmlParserFacade fromXmlParserFacade;
         AccountsProcessor accountsProcessor;
         ToXmlParser xmlParser;
 
         fromXmlParserFacade = FromXmlParserFacade.createDefaultFacade();
         accountsProcessor = new AccountsProcessor();
+
         //  Tutaj wybieramy parser w zaleznosci od wymaganej deklaracji w XML
         xmlParser = new JacksonDeclarationParser();
 //        xmlParser = new CustomDeclarationParser();
 
         try {
-            inputFilePath = "input.xml";
-            outputFilePath = "output.xml";
             List<Account> accountsFromFile = fromXmlParserFacade.getAccountsListFromXmlFile(inputFilePath);
             Set<Account> validSortedAccounts = accountsProcessor.validateAndSortAccounts(accountsFromFile);
             AccountListWrapper accountListWrapper = new AccountListWrapper(validSortedAccounts);
